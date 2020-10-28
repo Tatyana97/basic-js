a90bf94523a667160976509974b3c3e3ed71fec4
Tatyana97 <tanyusha.miranovich.97@mail.ru>
2020-10-28 12:40:01 +0300

    feat: add solution What season?

--- src/what-season.js
+++ src/what-season.js
@@ -1,22 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
+  
+  if (date == undefined) return "Unable to determine the time of year!";
+  if (Object.prototype.toString.call(date) !== "[object Date]") throw new Error();
+  
   let month = date.getMonth();
-  if (date == " "){
-    return 'Unable to determine the time of year!'
-  } else if (isNaN(date)){
-  return Error
-  }
-  
-  
   if (month >= 2 && month <= 4) {
       return 'spring';
     } else if (month >= 5 && month <= 7) {
       return 'summer';
     } else if (month >= 8 && month <= 10) {
-      return 'fall';
+      return 'autumn';
     } else {
       return 'winter';
     }
-    console.log(month); 
+    // console.log(month); 
 };
