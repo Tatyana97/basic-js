d3097922efdf0f9c012c3b7afc904cd3fa3e40eb
Tatyana97 <tanyusha.miranovich.97@mail.ru>
2021-03-25 01:16:53 +0300

    feat: add extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, { repeatTimes = 1, separator = "+", addition = "", additionRepeatTimes = 1, additionSeparator = "|" }) {
+  if (str != null) {
+    str = str.toString();
+}
+let result= "";
+
+if (addition == undefined){
+    for (let i = 0; i < repeatTimes; i++) {
+      result += str;
+      if (i == repeatTimes - 1) {
+					continue;
+      }
+      result += separator;
+    }
+    console.log(result);
+}
+
+    
+let additionStr1= "";
+
+   if (addition !== undefined || addition !== null) {
+   let additionStr = [];
+                addition = addition.toString();
+             
+  for (i = 0; i < additionRepeatTimes; i++) {
+    additionStr.push(addition);
+    if (i !== additionRepeatTimes - 1) {
+      additionStr.push(additionSeparator);
+    }
+    additionStr1 =  additionStr.join("");
+  }
+     /* console.log(additionStr1) */;
+     for (let i = 0; i < repeatTimes; i++) {
+       result = result + str + additionStr1 
+             if (i == repeatTimes - 1) {
+					continue;
+      }
+      result += separator;   
+     }
+    
+}
+return result
+// console.log(result);  
 };
+
+
   
