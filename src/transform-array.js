d6e6f9f96d143b18ae0fb387060b3b784bf778c8
Tatyana97 <tanyusha.miranovich.97@mail.ru>
2021-03-29 20:47:26 +0300

    feat: add transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,31 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  if (!Array.isArray(arr)) throw new Error();
-  if (arr[0] === undefined) return [];
-  
-  arr.forEach(element =>{
-  //  console.log(element)
-  
-  if(element === '--discard-next'){
-    let i = arr.indexOf('--discard-next');
-     arr.splice (i+1, 1);
-      
-  } else if(element === '--double-prev'){
-    let i = arr.indexOf('--double-prev');
-    arr.push (arr[i-1]);
-  }   else if(element === '--discard-prev'){
-    let i = arr.indexOf('--discard-prev');
-     arr.splice (i-1, 1);
-  }
-  else if(element === '--double-next'){
-    let i = arr.indexOf('--double-next');
-      arr.splice(i, 0, arr[i+1])
-  }
-  
-  })
-  
-  return arr.filter(val => {return val !== '--discard-next' && val !== '--discard-prev' && val !== '--double-next' && val !== '--double-prev' });
+  if (!(Array.isArray(arr))) throw new Error();
 
+  let res = [];
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == '--discard-next') {
+        i++;
+    } else if (arr[i] == '--discard-prev') {
+      if (i === 0 || arr[i - 2] === "--discard-next") continue;
+          res.pop();
+        
+    } else if (arr[i] == '--double-next') {
+      if (i === arr.length - 1) continue;
+          res.push(arr[i+1]);
 
+    } else if (arr[i] == '--double-prev') {
+      if (i === 0 || arr[i - 2] === "--discard-next") continue;
+      res.push(arr[i - 1]);
+    } else{
+        res.push(arr[i]);
+    }
+}
+return res;
 };
